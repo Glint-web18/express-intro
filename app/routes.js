f7a5eba45eec8075783239bcde12f48a6d8a257d
Justin <jusglinton@gmail.com>
2021-11-17 23:01:37 -0500

    commit

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,12 @@ router = express.Router();
 router.get("/", (req, res) => {
   return res.render("../assets/views/homepage.pug") ; 
 })
+router.get("/generic", (req, res) => {
+  return res.render("../assets/views/generic.pug") ; 
+})
+router.get("/post/create", (req, res) => {
+  return res.render("../assets/views/post/create.pug") ; 
+})
 
 router.get("/pug", (req, res) => {
     return res.render("../assets/views/testing.pug",{
